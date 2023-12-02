f4461e111c463585b33893801c7e21edbe78c140
Itt0ushura <balashov580@gmail.com>
2023-12-02 18:48:39 +0200

    Freezed rotation by z axis of cue

--- Assets/Game/Scripts/CueLogic.cs
+++ Assets/Game/Scripts/CueLogic.cs
@@ -4,8 +4,7 @@ public class CueLogic : MonoBehaviour
 {
     
     [SerializeField] private Transform Cue;
-    //var for mouse controll
-    private Vector3 mouse;
+    private Vector3 mouse; //var for mouse controll
 
     void Start()
     {
@@ -19,15 +18,14 @@ public class CueLogic : MonoBehaviour
     }
     void Rotation()
     {
-        //convert mouse pos from pixels до xyz
-        mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //set y = white ball y level
-        mouse.y = transform.position.y;
-        //looking for ball direction
-        Vector3 ballDirection = transform.position - Cue.position;
-        //look on white ball
-        Cue.rotation = Quaternion.LookRotation(ballDirection, mouse);
-        //moving Cue to mouse pos
-        Cue.position = mouse;
+        mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition); //convert mouse pos from pixels до xyz
+        mouse.y = transform.position.y; //set y = white ball y level
+        Vector3 ballDirection = transform.position - Cue.position; //looking for ball direction
+        Quaternion cueRotation = Quaternion.LookRotation(ballDirection, mouse); //look on white ball
+        Cue.rotation = cueRotation; //rotation of cue around ball
+        Vector3 zFreeze = Cue.transform.eulerAngles; //freezing rotation of cue on 0 using euler angles
+        zFreeze.z = 0;
+        Cue.transform.eulerAngles = zFreeze;
+        Cue.position = mouse; //moving Cue to mouse pos
     }
 }
