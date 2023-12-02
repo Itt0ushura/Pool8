bb26d82d022e1234477b66ea8ef0c06c21a5ddaf
Itt0ushura <balashov580@gmail.com>
2023-12-02 19:03:23 +0200

    ballDirection set as global value for upcoming improvements

--- Assets/Game/Scripts/CueLogic.cs
+++ Assets/Game/Scripts/CueLogic.cs
@@ -5,7 +5,7 @@ public class CueLogic : MonoBehaviour
     
     [SerializeField] private Transform Cue;
     private Vector3 mouse; //var for mouse controll
-
+    private Vector3 ballDirection; //var for direction of ball
     void Start()
     {
 
@@ -20,12 +20,14 @@ public class CueLogic : MonoBehaviour
     {
         mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition); //convert mouse pos from pixels до xyz
         mouse.y = transform.position.y; //set y = white ball y level
-        Vector3 ballDirection = transform.position - Cue.position; //looking for ball direction
+
+        ballDirection = transform.position - Cue.position; //looking for ball direction
         Quaternion cueRotation = Quaternion.LookRotation(ballDirection, mouse); //look on white ball
         Cue.rotation = cueRotation; //rotation of cue around ball
         Vector3 zFreeze = Cue.transform.eulerAngles; //freezing rotation of cue on 0 using euler angles
         zFreeze.z = 0;
         Cue.transform.eulerAngles = zFreeze;
+
         Cue.position = mouse; //moving Cue to mouse pos
     }
 }
