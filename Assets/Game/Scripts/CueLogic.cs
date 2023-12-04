9a90f370d4aaae79e450336a793e6db316b353dd
Itt0ushura <balashov580@gmail.com>
2023-12-04 19:34:56 +0200

    Complete 3.3 task

--- Assets/Game/Scripts/CueLogic.cs
+++ Assets/Game/Scripts/CueLogic.cs
@@ -1,33 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CueLogic : MonoBehaviour
 {
-    
+
     [SerializeField] private Transform Cue;
+    [SerializeField] List<Rigidbody> allBalls; //array with rbs of all balls
+    private Rigidbody ballRb;
     private Vector3 mouse; //var for mouse controll
     private Vector3 ballDirection; //var for direction of ball
+
+
     void Start()
     {
-
+        ballRb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
+        Strike();
         Rotation();
-        Debug.DrawLine(Cue.position, Cue.position + Cue.forward * 5, Color.blue);
     }
     void Rotation()
     {
-        mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition); //convert mouse pos from pixels до xyz
+        mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mouse.y = transform.position.y; //set y = white ball y level
 
-        ballDirection = transform.position - Cue.position; //looking for ball direction
-        Quaternion cueRotation = Quaternion.LookRotation(ballDirection, mouse); //look on white ball
-        Cue.rotation = cueRotation; //rotation of cue around ball
+        ballDirection = transform.position - Cue.position;
+        Cue.rotation = Quaternion.LookRotation(ballDirection, mouse); //look on white ball
         Vector3 zFreeze = Cue.transform.eulerAngles; //freezing rotation of cue on 0 using euler angles
         zFreeze.z = 0;
         Cue.transform.eulerAngles = zFreeze;
-
         Cue.position = mouse; //moving Cue to mouse pos
+    }
+    void Strike()
+    {
+        if (IsStopped() && Input.GetMouseButtonUp(0))
+        {
+            ballRb.AddForce(ballDirection.normalized * 6f, ForceMode.Impulse);
+            StartCoroutine(StopTimer(10f));
+        }
+
+    }
+    bool IsStopped()
+    {
+        foreach (var ball in allBalls)
+        {
+            if (ball.velocity.magnitude > 0.3) //if at least one of balls is having more than .3 velocity - they are on move
+            {
+                Cue.gameObject.SetActive(false);
+                return false;
+            }
+        }
+        Cue.gameObject.SetActive(true);
+        return true;
+    }
+    IEnumerator StopTimer(float time) //timer for stop motion of balls
+    {
+        yield return new WaitForSeconds(time);
+        foreach (var ball in allBalls)
+        {
+            ball.velocity = Vector3.zero;
+            ball.angularVelocity = Vector3.zero;
+        }
     }
 }
